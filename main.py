e8f5a3529ac09346be65388ed7dded7a687248b6
Sayg <jacob.heehrle@gmail.com>
2025-02-20 10:27:41 +0100

    Uppgift A

--- main.py
+++ main.py
@@ -1,54 +1,44 @@
 import numpy as np
-import random
-import matplotlib.pyplot as plt
-def dela(A, v0, n):
-    result = np.zeros((2, n))
-    result[:,0] = v0
 
-    for k in range(1,n):
-        result[:,k] = A@result[:,k-1]
-    
-    return result
-
+C0 = 31
+L0 = 42
+U0 = 35
+n = 10
+V0 = np.matrix([[C0], [L0], [U0]])
 
 """
-Genererar IFS fraktaler
-"""
-def delb(v0, n):
-    A1 = np.array([[0, 0], [0, 0.16]])
-    A2 = np.array([[0.85, 0.04], [-0.04, 0.85]])
-    A3 = np.array([[0.2, -0.26], [0.23, 0.22]])
-    A4 = np.array([[-0.15, 0.28], [0.26, 0.24]])
+def ARecursive(C0, L0, U0, n):
+    index = 0
+    A = np.array([[0.7, 0.1, 0.2], [0.1, 0.6, 0.3], [0.3, 0.2, 0.5]])
+    Vn_1 = np.array([[C0], [L0], [U0]])
 
-    b1 = np.array([0, 0])
-    b2 = np.array([0, 1.6])
-    b3 = np.array([0, 1.6])
-    b4 = np.array([0, 0.44])
+    while (index < n):
 
-    result = np.zeros((2, n))
-    result[:,0] = v0
-
-    for k in range(1,n):
-        index = random.randrange(1, 100)
-        b = np.zeros((1, 1))
+        Vn = A @ Vn_1
+        Vn_1 = Vn
         
-        if index == 1:
-            b = b1
-            A = A1
-        elif index >= 2 and index < 86:
-            b = b2
-            A = A2
-        elif index >= 86 and index < 94:
-            b = b3
-            A = A3
-        elif index >= 94 and index <= 100:
-            b = b4
-            A = A4
+        index += 1
+        print(str(Vn) + "\n")
 
-        result[:,k] = A@result[:,k-1] + b
+    print ("Slutgiltig matris: " + str(Vn))
+"""
+
+def UppgiftA(V0, n):
+    A = np.matrix([[0.7, 0.1, 0.2], [0.1, 0.6, 0.3], [0.3, 0.2, 0.5]])
+    print(f"A = {str(A)} \n")
+    print(f"V0 = {str(V0)} \n")
+
+    poweredA = np.linalg.matrix_power(A, n)
     
-    return result
+    result = poweredA @ V0
+    print(f"V{n}= {str(result)} \n")
 
-test = delb(np.array([1,1]), 1000)
-plt.scatter(test[0],test[1])
-plt.show()
+def UppgiftB():
+    print(f"1 vecka: ")
+
+
+
+#ARecursive(C0, L0, U0, n)
+UppgiftA(V0, n)
+
+#Vid fallet där C0 = L0 = U0 = B, där B tillhör naturliga talen, kommer resulterande matris Vn förbli V0; Vn = V0
