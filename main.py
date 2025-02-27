f76e7b9a9ad634c2e11233c74a6bd8dcbda0001b
Jabba <87302129+jabb4@users.noreply.github.com>
2025-02-27 11:55:34 +0100

    done

--- main.py
+++ main.py
@@ -3,32 +3,36 @@ from scipy import linalg as LA
 from sympy import Matrix
 import time
 
-def solve_using_rref(matrix, vector_y=Matrix([[]])):
+def solve_using_rref(matrix, vector_y):
     matrix = Matrix(matrix)
     vector_y = Matrix(vector_y)
-    trappstegsform = matrix.rref()[0]
-    pivotelement = matrix.rref()[1]
+    matrix = matrix.row_join(vector_y)
+    trappstegsform, pivotelement = matrix.rref()
     nr_colums = len(trappstegsform.row(1))
+
+    y_factor = []
+    for i in range(nr_colums-1):
+        if i in pivotelement:
+            y_factor.append(trappstegsform.row(i)[-1])
+        else:
+            y_factor.append(0)
 
     beronde_vars = []
     for i1, e1 in enumerate(pivotelement):
         tmp = []
 
-        for i2, e2 in enumerate(trappstegsform.row(i1)):
+        for i2, e2 in enumerate(trappstegsform.row(i1)[:-1]):
             if i2 != e1:
                 tmp.append(-e2)
             else:
                 tmp.append(0)
         beronde_vars.append(tmp)
 
-    ## Check if the only solution is x=0
-    if sum(list(map(lambda x: sum(x), beronde_vars))) == 0:
-        return 0, "x = 0"
 
     fria_vars = []
-    for varnr in range(len(beronde_vars),nr_colums):
+    for varnr in range(len(beronde_vars),nr_colums-1):
         tmp = []
-        for i in range(nr_colums):
+        for i in range(nr_colums-1):
             if i+1 <= len(beronde_vars):
                 tmp.append(beronde_vars[i][varnr])
             elif i == varnr:
@@ -36,10 +40,7 @@ def solve_using_rref(matrix, vector_y=Matrix([[]])):
             else:
                 tmp.append(0)
         fria_vars.append(tmp)
-    
-    if not fria_vars:
-        for var in beronde_vars:
-            pass
+
 
     alphbet_without_x = "abcdefghijklmnopqrstuvwyz"*100
     var_names = []
@@ -47,33 +48,47 @@ def solve_using_rref(matrix, vector_y=Matrix([[]])):
     for var in range(nr_colums):
         if not alphbet_without_x[var] + str(index) in var_names:
             var_names.append(alphbet_without_x[var] + str(index))
-    
-    print(max(list(map(lambda x: max(x, key=lambda x: len(str(x))), fria_vars)), lambda x: len(x)))
+
     nice_string = []
-    #max_row_len = len(str(max(list(map(lambda x: max(x, key=lambda x: len(str(x))), fria_vars)),lambda x:len(str(x)))))
-    for row in range(nr_colums):
+    ## Check if the only solution is x=0
+    if sum(list(map(lambda x: sum(x), beronde_vars))) == 0:
+        max_row_len = len(str(round(max(y_factor, key=lambda x: len(str(round(x,3)))), 3)))
+        for i, e in enumerate(y_factor):
+            tmp = ""
+            if i+1 == round((len(y_factor)-1)/2):
+                tmp += "  x  =  "
+            else:
+                tmp += "        "
+            tmp += f"({str(round(e, 3)):^{max_row_len+1}})"
+            nice_string.append(tmp)
+
+        return y_factor, nice_string
+
+    max_row_len = len(max(list(map(lambda x: str(max(x, key=lambda x: len(str(x)))), fria_vars)), key=lambda x: len(x)))
+
+    for row in range(nr_colums-1):
         tmp = ""
-        for var in range(len(fria_vars)):
-            if row == round(nr_colums/2):
-                if var == 0:
-                    tmp += f"x =   {var_names[var]} "
+        for var in range(len(fria_vars)+1):
+            if var+1 != len(fria_vars)+1:
+                if row == round((nr_colums-1)/2):
+                    if var == 0:
+                        tmp += f"x =  {var_names[var]} "
+                    else:
+                        tmp += f"  +  {var_names[var]} "
                 else:
-                    tmp += f"   +  {var_names[var]} "
+                    tmp += "        "
+                tmp += f"({str(fria_vars[var][row]):^{max_row_len+1}})"
+
+            elif max(y_factor) == 0 and min(y_factor) == 0:
+                pass
             else:
-                tmp += "         "
-            row_len = len(str(fria_vars[var][row]))
-            if row_len == max_row_len:
-                tmp += f"( {fria_vars[var][row]} )"
-            elif (max_row_len - row_len) % 2 == 1:
-                tmp += f"( {(round(max_row_len/2)-1)*" "}{fria_vars[var][row]}{(round(max_row_len/2)-2)*" "} )"
-            else:
-                tmp += f"( {(round(max_row_len/2)-1)*" "}{fria_vars[var][row]}{(round(max_row_len/2)-1)*" "} )"
-        if vector_y:
-            if row == round(nr_colums/2):
-                tmp += f"   -     "
-            else:
-                tmp += "         "
-            tmp += f"( {(2-len(str(vector_y[row])))*" "}{vector_y[row]}{(2-len(str(vector_y[row])))*" "} )"
+                if row == round((nr_colums-1)/2):
+                    tmp += f"  +   "
+                else:
+                    tmp += "      "
+                tmp += f"({str(y_factor[row]):^{max_row_len+1}})"
+
+
         nice_string.append(tmp)
     
     
@@ -96,21 +111,24 @@ def solve_using_inverse(A, y):
 
 def uppgiftA():
     A = Matrix([[1,2,1,-1,2], [3,4,5,2,0], [2,2,1,0,2]])
+    y = Matrix([[0], [0], [0]])
     print("Uppgift A:")
-    for line in solve_using_rref(A)[1]:
+    for line in solve_using_rref(A, y)[1]:
         print(line)
-    print("\n\n")
+    print("\n")
 
 
 def uppgiftB(A, y, Aint, yint):
     print("Uppgift B med rand():")
     t=time.time()
-    print(solve_using_rref(A, y)[1])
+    for line in solve_using_rref(A, y)[1]:
+        print(line)
     print("Time taken on rand():", time.time()-t, "\n\n")
 
     print("Uppgift B med randint():")
     t=time.time()
-    print(solve_using_rref(Aint, yint)[1])
+    for line in solve_using_rref(Aint, yint)[1]:
+        print(line)
     print("Time taken on randint():", time.time()-t, "\n\n")
 
 def uppgiftC(A, y, Aint, yint):
@@ -125,15 +143,15 @@ def uppgiftC(A, y, Aint, yint):
     print("Time taken on randint():", time.time()-t)
 
 def main():
-    A = np.random.rand(100, 100)
-    Aint = np.random.randint(0, 100, size=(100, 100))
+    A = np.random.rand(5, 5)
+    Aint = np.random.randint(0, 100, size=(5, 5))
 
-    y = np.random.rand(100)
-    yint = np.random.randint(0, 100, size=(100))
+    y = np.random.rand(5)
+    yint = np.random.randint(0, 100, size=(5))
 
     uppgiftA()
-    #uppgiftB(A, y, Aint, yint)
-    #uppgiftC(A, y, Aint, yint)
+    uppgiftB(A, y, Aint, yint)
+    uppgiftC(A, y, Aint, yint)
 
 if __name__ == "__main__":
     main()
